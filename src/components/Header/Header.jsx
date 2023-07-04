c2bb7fb48fd3ce73285412a8f807b356affb9009
rivindu2 <rivindu@incubatelabs.com>
2023-07-04 10:17:25 +0530

    Add

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,97 @@
+import React, { useRef } from "react";
+import { Container } from "reactstrap";
+import "./header.css";
+import { Link } from "react-scroll";
+import AboutUs from "../About-us/AboutUs";
+const navLinks = [
+  {
+    display: "Home",
+    url: "#",
+  },
+  {
+    display: "About",
+    url: "/AboutUs"
+  },
+
+  {
+    display: "Courses",
+    url: "#",
+  },
+  {
+    display: "Pages",
+    url: "#",
+  },
+  {
+    display: "Blog",
+    url: "#",
+  },
+];
+
+
+const Header = () => {
+  const menuRef = useRef();
+
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <h2 className=" d-flex align-items-center gap-1">
+              <i className="ri-pantone-line"></i> Xenia.
+            </h2>
+          </div>
+
+          <div className="nav d-flex align-items-center gap-5">
+            <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
+              <ul className="nav__list">
+
+                <li>
+                  <Link  style={{ fontWeight: "bold"}} activeClass="active" to="#" spy={true} smooth={true}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link  style={{ fontWeight: "bold"}} to="aboutUs" spy={true} smooth={true}>
+                    About
+                  </Link>
+                </li>
+                    <li>
+                      <Link  style={{ fontWeight: "bold"}} to="aboutUs" spy={true} smooth={true}>
+                        d
+                      </Link>
+                    </li>
+                    <li>
+                      <Link  style={{ fontWeight: "bold"}} to="aboutUs" spy={true} smooth={true}>
+                        v
+                      </Link>
+                    </li>
+                    <li>
+                      <Link  style={{ fontWeight: "bold"}} to="aboutUs" spy={true} smooth={true}>
+                        ds
+                      </Link>
+                    </li>
+
+              </ul>
+            </div>
+
+            <div className="nav__right">
+              <p className="mb-0 d-flex align-items-center gap-2">
+                <i className="ri-phone-line"></i> +88 0123456789
+              </p>
+            </div>
+          </div>
+
+          <div className="mobile__menu">
+            <span>
+              <i className="ri-menu-line" onClick={menuToggle}></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero-Section/HeroSection.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../../assests/images/hero-img1.png";
+import "./hero-section.css";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="hero__content">
+              <h2 className="mb-4 hero__title">
+                Personalize <br/> Your customer interaction <br/> on Every Path
+              </h2>
+              <p className="mb-5">
+                Personalization is complicated. Our technology makes it simple.
+                Show millions of customers the right product, in the right place, at the right time.
+              </p>
+            </div>
+
+          </Col>
+
+          <Col lg="6" md="6">
+            <img src={heroImg} alt="" className="w-100 hero__img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
